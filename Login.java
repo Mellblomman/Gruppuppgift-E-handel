8b4372c5493f6e5c674ba4c9d9b93f77b543bd8c
Mellblomman <115022366+Mellblomman@users.noreply.github.com>
2023-09-27 14:46:57 +0200

    Changes made to login class

--- Login.java
+++ Login.java
@@ -4,7 +4,7 @@ public class Login
     //Enter username
     //Enter password
 
-    public static void main(String[] args)
+    public void loginMenu()
     {
         Scanner scan = new Scanner(System.in);
 
@@ -12,19 +12,24 @@ public class Login
                 "\n2. Customer");
         int inputNumForChoice = scan.nextInt();
 
-        if(inputNumForChoice == 1)
+        if (inputNumForChoice == 1)
         {
             System.out.println("Enter admin username:");
             String inputAdminUsernameForLogin = scan.nextLine();
             System.out.println("Enter admin password:");
             String inputAdminPasswordForLogin = scan.nextLine();
         }
-        else if(inputNumForChoice == 2)
+        else if (inputNumForChoice == 2)
         {
             System.out.println("Enter your social security number:");
             int inputSSNForLogin = scan.nextInt();
             System.out.println("Enter your password");
             String inputCustomerPasswordForLogin = scan.nextLine();
         }
+        else
+        {
+            System.out.println("Please choose one of the two options above.");
+        }
     }
+
 }
