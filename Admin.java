a162391272cacab7d83a57484f6fd65fe85a92e9
Lukas <lukas.engdahl@homtail.com>
2023-09-27 14:05:54 +0200

    Admin class. Hardcoded login password + username.

---
+++ Admin.java
@@ -0,0 +1,13 @@
+public class Admin {
+    private String name = "admin";
+    private String password = "mlssadmin";
+
+    private Admin(){}
+    public String getName() {
+        return name;
+    }
+    public String getPassWord() {
+        return password;
+    }
+
+}
