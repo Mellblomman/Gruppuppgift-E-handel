66525cbb7ca139e61b95794404b97a1103b5cc9b
Vedmastaren <117941745+Vedmastaren@users.noreply.github.com>
2023-09-28 18:15:48 +0200

    created while loop in logInAdminMethod to check for username and password. Fixed the an if else statement to see if the admin username equals our string, same with password.

--- src/Admin.java
+++ src/Admin.java
@@ -4,53 +4,54 @@ import java.util.Scanner;
 
 public class Admin {
     Scanner scan = new Scanner(System.in);
-    private String name = "admin";
-    private String password = "mlssadmin";
 
-    Admin(){}
-    public String getName() {
-        return name;
+    private String adminUserName = "admin";
+    private String adminPassword = "mlssadmin";
+
+    Admin() {
     }
-    public String getPassWord() {
-        return password;
-    }
-    public void logInAdmin(){
+
+    public void logInAdmin() {
+        boolean runAdminLogIn = true;
+        while (runAdminLogIn) {
+            System.out.println("Enter admin username:");
+            String inputAdminUsernameForLogin = scan.nextLine().toLowerCase();
+            System.out.println("Enter admin password:");
+            String inputAdminPasswordForLogin = scan.nextLine().toLowerCase();
 
 
-        System.out.println("Enter admin username:");
-        String inputAdminUsernameForLogin = scan.nextLine().toLowerCase();
-        System.out.println("Enter admin password:");
-        String inputAdminPasswordForLogin = scan.nextLine().toLowerCase();
-
-        if(inputAdminUsernameForLogin.equals(this.name) && inputAdminPasswordForLogin.equals(this.password))
-        {
-            boolean run = true;
-            while(run){
-                System.out.println("Pick a number." +
-                        "\n1. Product Management" +
-                        "\n2. Customer Information" +
-                        "\n3. Order/Transactions" +
-                        "\n4. Logout");
-                String menuChoice = scan.next();
-                switch(menuChoice) {
-                    case "1":
-                        System.out.println("Insert Product Management Method");
-                        break;
-                    case "2":
-                        System.out.println("Insert Customer Information Method");
-                        break;
-                    case "3":
-                        System.out.println("Insert Order/Transactions Method");
-                        break;
-                    case "4":
-                        System.out.println("Logout");
-                        run = false;
-                        Login loginmenu = new Login();
-                        loginmenu.loginMenu();
-                        break;
-                    default:
-                        System.out.println("Wrong input. Choose 1-4");
+            if (inputAdminUsernameForLogin.equals(adminUserName) && inputAdminPasswordForLogin.equals(adminPassword)) {
+                boolean runAdminMenu = true;
+                while (runAdminMenu) {
+                    System.out.println("Pick a number." +
+                            "\n1. Product Management" +
+                            "\n2. Customer Information" +
+                            "\n3. Order/Transactions" +
+                            "\n4. Logout");
+                    String menuChoice = scan.next();
+                    switch (menuChoice) {
+                        case "1":
+                            System.out.println("Insert Product Management Method");
+                            break;
+                        case "2":
+                            System.out.println("Insert Customer Information Method");
+                            break;
+                        case "3":
+                            System.out.println("Insert Order/Transactions Method");
+                            break;
+                        case "4":
+                            System.out.println("Logout");
+                            runAdminMenu = false;
+                            Login loginmenu = new Login();
+                            loginmenu.loginMenu();
+                            break;
+                        default:
+                            System.out.println("Wrong input. Choose 1-4");
+                    }
                 }
+            } else {
+                System.out.println("Please enter the admin username and password again");
+
             }
         }
     }
