b6d17e6e59e9b39dc8476c7d252a0b114bc611c4
Vedmastaren <117941745+Vedmastaren@users.noreply.github.com>
2023-09-28 10:54:45 +0200

    Removed Customer from Main.

--- Main.java
+++ Main.java
@@ -3,7 +3,6 @@ import java.io.IOException;
 public class Main
 {
     public static void main(String[] args) throws IOException {
-        Customer customer = new Customer();
         Customers customers = new Customers();
         Login login = new Login();
         login.loginMenu();
