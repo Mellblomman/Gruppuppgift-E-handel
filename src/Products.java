100563896eccadd1fd617e865b89028019c3d833
Salah <sallad_1@live.com>
2023-10-03 11:22:55 +0200

    Changed all indentations. Moved createFileWithProducts to constructor.

--- src/Products.java
+++ src/Products.java
@@ -2,33 +2,16 @@ package src;
 
 import java.io.*;
 import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 
 public class Products {
-        public ArrayList<Product> productsList = new ArrayList<Product>();
-        public String productFileName = "products.txt";
-        Products() {
+    public ArrayList<Product> productsList = new ArrayList<Product>();
+    public String productFileName = "products.txt";
 
-        }
-        public ArrayList<Product> getProductsList() { return productsList; }
-        public boolean createFileWithProducts() {       //Calling this method in admin when product is created.
-            File file = new File("products.txt");
-
-            try {
-                if (file.createNewFile()) {
-                    System.out.println("File have been created: " + file.getName());
-                } else {
-                    System.out.println("File " + file.getName() + " already exists!");
-                    return false;
-                }
-            } catch (IOException e) {
-                System.out.println("Something went wrong when creating a txt-file!");
-            }
-            return true;
-        }
-
-        public void readProductsFromFile() {
+    public Products() {
+        if (!createFileWithProducts()) {
             try {
                 Scanner scan = new Scanner(new File(productFileName));
                 while (scan.hasNextLine()) { //if file already exist, a Scanner will read every line of the file and seperate with ", "
@@ -38,34 +21,48 @@ public class Products {
                     Product tempProducts = new Product( //creating a product object, with split values and this object will be added to products list
                             productsInfo[0],
                             productsInfo[1],
-                            Integer.parseInt(productsInfo[2])
-
-                    );
+                            Integer.parseInt(productsInfo[2]));
                     productsList.add(tempProducts); //added to productList
                 }
             } catch (FileNotFoundException e) {
                 System.out.println("Wrong!" + e.getMessage());
             }
         }
+    }
 
-        public boolean addProductToTextFile(Product newProduct)  {
+    public boolean createFileWithProducts () {  //Calling this method in admin when product is created.
+        File file = new File(productFileName);
 
-            try{
-                FileOutputStream fos = new FileOutputStream(this.productFileName, true); //fos created to write data to the file productFileName
-                PrintStream printStream = new PrintStream(fos); //append: true indicates that data should be appended to the file if it already exists
-
-                printStream.print("\n" + newProduct.formatedStringForFile()); //print the products information to the file
-                System.out.println(newProduct.formatedStringForFile());
-
-                fos.close();
-                printStream.close();
-                return true;                    //if it works without exceptions it will be true, else false!
-            } catch (Exception e){
-                System.out.println("Product added successfully." + e.getMessage());
+        try {
+            if (file.createNewFile()) {
+                System.out.println("File have been created: " + file.getName());
+                return true;
             }
-
-            return false;
+        } catch (IOException e) {
+            System.out.println("Something went wrong when creating a txt-file!");
         }
+        return false;
+    }
+
+
+    public boolean addProductToTextFile (Product newProduct){
+
+        try {
+            FileOutputStream fos = new FileOutputStream(this.productFileName, true); //fos created to write data to the file productFileName
+            PrintStream printStream = new PrintStream(fos); //append: true indicates that data should be appended to the file if it already exists
+
+            printStream.print("\n" + newProduct.formatedStringForFile()); //print the products information to the file
+            System.out.println(newProduct.formatedStringForFile());
+
+            fos.close();
+            printStream.close();
+            return true;                    //if it works without exceptions it will be true, else false!
+        } catch (Exception e) {
+            System.out.println("Product added successfully." + e.getMessage());
+        }
+
+        return false;
+    }
     public void removeProductFromTextFile(Product product) {
         try {
             Scanner reader = new Scanner(new File(productFileName));
@@ -91,12 +88,11 @@ public class Products {
                 System.out.println("product not found");
             }
         } catch (Exception e) {
-            System.out.println("Something went wrong when we removed Product from the file ");
-            e.printStackTrace();
+            System.out.println("Something went wrong when we removed Product from the file" + e.getMessage());
         }
     }
 
-    public void printProducts() {
+    public void printProducts () {
         try {
             // Specify the path to the products.txt file
             String filePath = "products.txt";
